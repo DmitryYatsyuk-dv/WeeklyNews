b362f51ccd88a9bba709646f504feb53de84402d
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2021-03-22 22:19:51 +0300

    Done:	custom tableViewCell; Todo: 	Needed put all ours views these together inside our view controller and 	construct our table view.

---
+++ WeeklyNews/Views/NewsTableViewCell.swift
@@ -0,0 +1,76 @@
+//
+//  NewsTableViewCell.swift
+//  WeeklyNews
+//
+//  Created by Lucky on 22.03.2021.
+//
+
+import UIKit
+
+final class NewsTableViewCell: UITableViewCell {
+    
+    var newsViewModel: NewsViewModel? {
+        didSet {
+            if let newsViewModel = newsViewModel {
+                
+            }
+        }
+    }
+    
+    var newsImageData: Data? {
+        didSet {
+            if let data = newsImageData {
+                newsImage.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    private lazy var newsImage: ShadowImageView = {
+        let iv = ShadowImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        addSubview(titleLabel)
+        addSubview(newsImage)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        
+        // News image
+        NSLayoutConstraint.activate([
+            newsImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            newsImage.trailingAnchor.constraint(equalTo: trailingAnchor),
+            newsImage.topAnchor.constraint(equalTo: topAnchor),
+            newsImage.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        // Title
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+        ])
+    }
+}
